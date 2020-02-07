c30fc68f33e20c6c236d157fa567687fe9c41925
Ekampreet Singh <ekampreet.singh@block8.com>
2020-02-07 13:27:09 +0530

    code lines shuffle

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
 app.get("/url", (req, res, next) => {
   res.json(["Ekam", "Navdeep"]);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
