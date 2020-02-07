b95ad7c017f7cf40d885943af4ac27f1bb70c272
Ekampreet Singh <ekampreet.singh@block8.com>
2020-02-07 14:17:21 +0530

    changed port to 5000

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get("/url", (req, res, next) => {
   res.json(["Ekam", "Navdeep"]);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
